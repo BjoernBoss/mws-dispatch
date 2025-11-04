35da7e33a46f61c73b1061d8d64ce6fe2aa690a3
Bjoern Boss Henrichsen <bjoernbossdev@gmail.com>
2025-11-04 21:42:56 +0100

    Update to newest interface and add logging

--- path-dispatch.ts
+++ path-dispatch.ts
@@ -3,6 +3,7 @@
 import * as libCommon from "core/common.js";
 import * as libClient from "core/client.js";
 import * as libLocation from "core/location.js";
+import * as libLog from "core/log.js";
 
 /*
 *	Dispatch modules to live at separate sub-paths
@@ -11,10 +12,13 @@ import * as libLocation from "core/location.js";
 export class PathDispatch implements libCommon.ModuleInterface {
 	private mapping: Record<string, libCommon.ModuleInterface>;
 
+	public name: string = 'path-dispatch';
 	constructor(mapping: Record<string, libCommon.ModuleInterface>) {
 		this.mapping = {};
-		for (const path in mapping)
+		for (const path in mapping) {
+			libLog.Log(`Mapping [${mapping[path].name}] to [${path}]`);
 			this.mapping[libLocation.Sanitize(path)] = mapping[path];
+		}
 	}
 
 	private dispatch(client: libClient.HttpBaseClass): libCommon.ModuleInterface | null {
@@ -30,7 +34,7 @@ export class PathDispatch implements libCommon.ModuleInterface {
 
 		/* check if a handler has been found and translate the path accordingly */
 		if (bestMatch != null) {
-			client.log(`Client dispatched to handler [${bestMatch}]`);
+			client.log(`Client dispatched to handler [${this.mapping[bestMatch].name}] at [${bestMatch}]`);
 			client.translate(bestMatch);
 			return this.mapping[bestMatch];
 		}
